0db4a8135669257428d8a56bd382d98b0f88cd1f
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 10:23:54 +0530

    Success: Wage For Month

--- EmpWage.java
+++ EmpWage.java
@@ -2,35 +2,36 @@
 public class EmpWage {
 
 public static void main(String[] args) {
-		public static void main(String[] args) {
-		// TODO Auto-generated method stub
+// TODO Auto-generated method stub
 		   int isFullTime=1;
 		   int isPartTime=2;
 		   int isAbsent=0;
 		   int wageperhour=20;
 		   int totalhours;
 		   int wage=0;
+		   int sumofsalary=0;
+		   for (int i=0; i < 20; i++)
+	       {
 	       double attendance=Math.floor(Math.random()*10)%3;
 	       int value = (int) attendance;
 	       switch(value)
 	       {
 	       case 1:
 	    	   totalhours=16;
-	        	System.out.println("Employee is Full Time");
 	        	wage=wageperhour*totalhours;
-	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	sumofsalary=sumofsalary+wage;
 	        	break;
 	       case 2:
 	    	   totalhours=8;
-	        	System.out.println("Employee is Part Time");
 	        	wage=wageperhour*totalhours;
-	        	System.out.println("Part Time Day Wage:"+ wage);
+	        	sumofsalary=sumofsalary+wage;
 	        	break;
 	       case 0:
-	    	   System.out.println("Employee is Absent");
-	        	System.out.println("Day Wage:"+ wage);
+	    	    sumofsalary=sumofsalary+wage;
 	        	break;
 	       }
+	    }
+		   System.out.println("Total Monthly Salary (20 days) which include parttime,fulltime and absence is:" + sumofsalary);
 	}
 
 
