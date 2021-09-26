46034367ee66032079ea7d03f6eb34cc5bf90459
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 10:15:00 +0530

    Success: Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,19 +1,28 @@
 
 public class EmpWage {
 
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        int isPresent=1;
-        double attendance=Math.floor(Math.random()*10)%2;
-        if (attendance == isPresent)
-        {
-        	System.out.println("Employee is Present");
-        }
-        else 
-        {
-        	System.out.println("Employee is Absent");
-        }
+		   int isPresent=1;
+		   int wageperhour=20;
+		   int totalhours=8;
+		   int wage=0;
+	        double attendance=Math.floor(Math.random()*10)%2;
+	        if (attendance == isPresent)
+	        {
+	        	System.out.println("Employee is Present");
+	        	wage=wageperhour*totalhours;
+	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	
+	        }
+	        else 
+	        {
+	        	System.out.println("Employee is Absent");
+	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	
+	        }
 	}
+
 
 }
 
