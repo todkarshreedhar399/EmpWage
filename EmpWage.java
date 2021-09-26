facaf408324f14d3cdb018b426e9e07da052b8d9
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 11:10:59 +0530

    Success: To Write Class Method To Compute Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class EmployeeWage {
+public class EmpWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
