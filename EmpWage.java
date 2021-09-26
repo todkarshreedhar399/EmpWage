1509982c189d416956da8fed18c5eec93c4aac24
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 10:26:06 +0530

    Success: Wage Till Condition

--- EmpWage.java
+++ EmpWage.java
@@ -7,33 +7,48 @@ public static void main(String[] args) {
 		   int isPartTime=2;
 		   int isAbsent=0;
 		   int wageperhour=20;
-		   int totalhours;
+		   int hours;
+		   int totalhours=0;
 		   int wage=0;
 		   int sumofsalary=0;
 		   for (int i=0; i < 20; i++)
 	       {
 	       double attendance=Math.floor(Math.random()*10)%3;
 	       int value = (int) attendance;
+	       while(totalhours<90)
+	       {
 	       switch(value)
 	       {
 	       case 1:
-	    	   totalhours=16;
-	        	wage=wageperhour*totalhours;
+	    	   if(totalhours<100)
+	    	   {
+	    	    hours=16;
+	        	wage=wageperhour*hours;
 	        	sumofsalary=sumofsalary+wage;
+	        	totalhours=totalhours+hours;
+	    	   }
 	        	break;
 	       case 2:
-	    	   totalhours=8;
-	        	wage=wageperhour*totalhours;
+	    	   if(totalhours<100)
+	    	   {
+	    	   hours=6;
+	        	wage=wageperhour*hours;
 	        	sumofsalary=sumofsalary+wage;
+	        	totalhours=totalhours+hours;
+	    	   }
 	        	break;
 	       case 0:
 	    	    sumofsalary=sumofsalary+wage;
 	        	break;
 	       }
+	      }
 	    }
+		   
+		 
+		   System.out.println("Total Hours of Month:" + totalhours);
 		   System.out.println("Total Monthly Salary (20 days) which include parttime,fulltime and absence is:" + sumofsalary);
+		   
 	}
-
 
 }
 
