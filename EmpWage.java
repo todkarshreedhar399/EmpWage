7c60fcb028bd970be8af736ac383ebe55f181c53
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 10:21:28 +0530

    Success: Switch Case

--- EmpWage.java
+++ EmpWage.java
@@ -2,6 +2,7 @@
 public class EmpWage {
 
 public static void main(String[] args) {
+		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		   int isFullTime=1;
 		   int isPartTime=2;
@@ -9,29 +10,29 @@ public static void main(String[] args) {
 		   int wageperhour=20;
 		   int totalhours;
 		   int wage=0;
-	        double attendance=Math.floor(Math.random()*10)%3;
-	        if (attendance == isFullTime)
-	        {
-	        	totalhours=16;
+	       double attendance=Math.floor(Math.random()*10)%3;
+	       int value = (int) attendance;
+	       switch(value)
+	       {
+	       case 1:
+	    	   totalhours=16;
 	        	System.out.println("Employee is Full Time");
 	        	wage=wageperhour*totalhours;
 	        	System.out.println("Full Time Day Wage:"+ wage);
-	        	
-	        }
-	        else if (attendance == isPartTime)
-	        {
-	        	totalhours=8;
+	        	break;
+	       case 2:
+	    	   totalhours=8;
 	        	System.out.println("Employee is Part Time");
 	        	wage=wageperhour*totalhours;
 	        	System.out.println("Part Time Day Wage:"+ wage);
-	        	
-	        }
-	        else 
-	        {
-	        	System.out.println("Employee is Absent");
+	        	break;
+	       case 0:
+	    	   System.out.println("Employee is Absent");
 	        	System.out.println("Day Wage:"+ wage);
-	        	
-	        }
+	        	break;
+	       }
+	}
+
 
 }
 
