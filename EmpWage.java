b9fde8f13d4c6f33645051b0874cd484c9fd0d32
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 10:17:48 +0530

    Success: Part Time Full Time Calculations

--- EmpWage.java
+++ EmpWage.java
@@ -3,26 +3,35 @@ public class EmpWage {
 
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		   int isPresent=1;
+		   int isFullTime=1;
+		   int isPartTime=2;
+		   int isAbsent=0;
 		   int wageperhour=20;
-		   int totalhours=8;
+		   int totalhours;
 		   int wage=0;
-	        double attendance=Math.floor(Math.random()*10)%2;
-	        if (attendance == isPresent)
+	        double attendance=Math.floor(Math.random()*10)%3;
+	        if (attendance == isFullTime)
 	        {
-	        	System.out.println("Employee is Present");
+	        	totalhours=16;
+	        	System.out.println("Employee is Full Time");
 	        	wage=wageperhour*totalhours;
 	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	
+	        }
+	        else if (attendance == isPartTime)
+	        {
+	        	totalhours=8;
+	        	System.out.println("Employee is Part Time");
+	        	wage=wageperhour*totalhours;
+	        	System.out.println("Part Time Day Wage:"+ wage);
 	        	
 	        }
 	        else 
 	        {
 	        	System.out.println("Employee is Absent");
-	        	System.out.println("Full Time Day Wage:"+ wage);
+	        	System.out.println("Day Wage:"+ wage);
 	        	
 	        }
-	}
-
 
 }
 
