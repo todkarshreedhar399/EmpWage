61da517a003e54f8ea6eade5edef03c4b8abe001
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 11:10:10 +0530

    Success: To Write Class Method To Compute Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,61 +1,40 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
+package com.company;
 
-public class EmpWage implements IComputeWage{
-    public static final int isPartTime=0;
-    public static final int isFullTime=1;
-    private ArrayList<CompanyEmpWage> empList;
-    private LinkedList<Integer>dailyWage;
-    public EmployeeWage() {
-        empList=new ArrayList<>();
-        dailyWage=new LinkedList<>();
-    }
-    @Override
-    public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
-        CompanyEmpWage empWage=new CompanyEmpWage(company,empRatePerHour,numOfWorkingDays,maximumHoursPerMonth);
-        empList.add(empWage);
-    }
+public class EmployeeWage {
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
-    @Override
-    public void computeEmpWage() {
-        for(int i=0;i<empList.size();i++) {
-            empList.get(i).setTotalEmpWage(this.computeEmpWage(empList.get(i)));
-            System.out.println(empList.get(i));
-        }
-
-    }
-    HashMap<String,LinkedList> companyDailyWage=new HashMap<>();
-    private  int computeEmpWage(CompanyEmpWage companyEmpWage) {
-        int empHours=0, totalEmpHours=0, totalWorkingDays=0;
-        while(totalEmpHours<=companyEmpWage.maximumHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays){
-            int check=(int)(Math.floor(Math.random()*10)%2);
-            switch(check){
-                case isPartTime:
-                    empHours=4;
+    public static int computeEmpWage() {
+        System.out.println("Welcome To Employee Wage Computation");
+        // Variables
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+        // Computation
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+                totalWorkingDays < NUM_OF_WORKING_DAYS){
+            totalWorkingDays++;
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PART_TIME:
+                    empHrs = 4;
                     break;
-                case isFullTime:
-                    empHours=8;
+                case IS_FULL_TIME:
+                    empHrs = 8;
                     break;
-                default :
-                    empHours=0;
+                default:
+                    empHrs = 0;
             }
-            totalEmpHours+=empHours;
-            totalWorkingDays+=1;
-            dailyWage.add(empHours*companyEmpWage.empRatePerHour);
-
+            totalEmpHrs += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
         }
-        companyDailyWage.put(companyEmpWage.company, dailyWage);
-        System.out.println("Daily Wage for the "+companyEmpWage.company+" is :"+dailyWage);
-        dailyWage.clear();
-        return (totalEmpHours*companyEmpWage.empRatePerHour);
-
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+        return totalEmpWage;
     }
-    public static void main(String args[]) {
-        EmployeeWage employeeWage=new EmployeeWage();
-        employeeWage.addCompanyEmpWage("Dmart", 15, 21, 80);
-        employeeWage.addCompanyEmpWage("Reliance", 15, 18, 100);
-        employeeWage.computeEmpWage();
+    public static void main(String[] args) {
+        computeEmpWage();
     }
 
 }
--- IComputeWage.java
+++
@@ -1,5 +0,0 @@
-public interface IComputeWage {
-    public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maximumHoursPerMonth);
-    public void computeEmpWage();
-    public int getTotalWage(String company);
-}
