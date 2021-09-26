2f99624a74d7fd4796c025b8b4b98cfb40f2d31d
todkarshreedhar399 <87130907+todkarshreedhar399@users.noreply.github.com>
2021-09-26 11:23:13 +0530

    Update EmpWage.java

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,7 @@
 
 import java.util.ArrayList;
 
-public class EmployeeWage implements IComputeWage{
+public class EmpWage implements IComputeWage{
     public static final int isPartTime=0;
     public static final int isFullTime=1;
     private ArrayList<CompanyEmpWage> empList;
