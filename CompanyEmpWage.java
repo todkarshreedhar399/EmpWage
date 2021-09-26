1fc9266b3d177ed1d5bca8a9a6e362b13c1ebc17
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-09-26 10:42:25 +0530

    Success: Multiple Company Employee Wage

---
+++ CompanyEmpWage.java
@@ -0,0 +1,20 @@
+public class CompanyEmpWage {
+    public final String company;
+    public final int empRatePerHour;
+    public final int numOfWorkingDays;
+    public final int maximumHoursPerMonth;
+    public int totalWage;
+    public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maximumHoursPerMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maximumHoursPerMonth = maximumHoursPerMonth;
+    }
+    public void setTotalEmpWage(int totalWage){
+        this.totalWage=totalWage;
+    }
+    public String toString() {
+        return "Total Employee Wage for "+company+" is "+totalWage;
+    }
+}
+
